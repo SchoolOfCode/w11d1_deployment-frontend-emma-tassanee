37935d15b296f824f59a9d9cef33f90d984a85d7
Tassanee Atsawajuramanee <tassanee.atsa@gmail.com>
2022-07-04 16:34:16 +0100

    adding form testing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
+        <p>Hello World!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +16,21 @@ function App() {
           Learning to deploy an app on Netlify!
         </a>
       </header>
+      <form name="contact" netlify>
+        <p>
+          <label>
+            Name <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
   );
 }
